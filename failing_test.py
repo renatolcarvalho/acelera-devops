c663192a5d415b9becfb44081c8a8331a39a9af8
Renato Carvalho <renatoluizcarvalho@gmail.com>
2021-05-25 16:16:06 -0300

    Update failing_test.py

--- failing_test.py
+++ failing_test.py
@@ -7,7 +7,7 @@ class FailingTests(unittest.TestCase):
         with self.assertRaises(Exception) as context:
             failing.failing()
 
-        exceptionMessage = "There's something happening here. But what it is ain't exactly clear"
+        exceptionMessage = "There's something happening here. But what it is ain't exactly clear!"
         exception = context.exception
         self.assertEqual(exceptionMessage, str(exception))
 
